d2089d40d38f7783a86b1e94e2edc70b3c4d30cd
Daniel Higuita <daniel.higuita007@gmail.com>
2020-03-19 19:09:03 -0500

    regex

--- DFANode.py
+++
@@ -1,8 +0,0 @@
-class DFANode:
-    def __init__(self, nodeName, dfaNodeList):
-        self.nodeName = nodeName
-        self.dfaNodeList = dfaNodeList
-        
-    def setVariableValue(self, variableName, value):
-        if variableName in vars(self).keys():
-            vars(self)[variableName] = value
--- stateGraph.py
+++
@@ -1,8 +0,0 @@
-class StateGraph:
-    def __init__(self, initialState, finalState):
-        self.initialState = initialState
-        self.finalState = finalState
-        
-    def setVariableValue(self, variableName, value):
-        if variableName in vars(self).keys():
-            vars(self)[variableName] = value
